56b538cccd9ce93218fe1ac26968180b46631251
yalogames <yalogames@yalo.com>
2024-09-22 03:15:25 +0800

    full screen added with extra ads

--- games/games.js
+++ games/games.js
@@ -784,3 +784,60 @@ randomGames.forEach(element => {
 
 document.getElementById('moreGames').appendChild(divContainer);
 });
+
+// full screen
+var is_fullscreen = false;
+function open_fullscreen() {
+	let game = document.getElementById("iframe-box");
+	if(is_fullscreen){
+		// Exit fullscreen
+		is_fullscreen = false;
+		if(is_mobile_device()){
+			game.style.position = "absolute";
+			document.getElementById("mobile-back-button").style.display = "none";
+			document.getElementById("game-player").style.display = "none";
+		} else {
+			if (game.requestFullscreen) {
+				game.requestFullscreen();
+			} else if (game.mozRequestFullScreen) { /* Firefox */
+				game.mozRequestFullScreen();
+			} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+				game.webkitRequestFullscreen();
+			} else if (game.msRequestFullscreen) { /* IE/Edge */
+				game.msRequestFullscreen();
+			}
+		}
+	} else {
+		// Enter fullscreen
+		is_fullscreen = true;
+		if(is_mobile_device()){
+			document.getElementById("game-player").style.display = "block";
+			game.style.position = "fixed";
+			document.getElementById("mobile-back-button").style.display = "flex";
+		} else {
+			if (game.requestFullscreen) {
+				game.requestFullscreen();
+			} else if (game.mozRequestFullScreen) { /* Firefox */
+				game.mozRequestFullScreen();
+			} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+				game.webkitRequestFullscreen();
+			} else if (game.msRequestFullscreen) { /* IE/Edge */
+				game.msRequestFullscreen();
+			}
+		}
+	}
+};
+
+function is_mobile_device(){
+	if (navigator.userAgent.match(/Android/i)
+	|| navigator.userAgent.match(/webOS/i)
+	|| navigator.userAgent.match(/iPhone/i)
+	|| navigator.userAgent.match(/iPad/i)
+	|| navigator.userAgent.match(/iPod/i)
+	|| navigator.userAgent.match(/BlackBerry/i)
+	|| navigator.userAgent.match(/Windows Phone/i)) {
+		return true;
+	} else {
+		return false;
+	}
+}
